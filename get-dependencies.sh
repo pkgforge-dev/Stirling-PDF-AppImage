051446a13e5d582a3290d3c52b6e60ab6a912eda
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-19 16:40:59 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,10 +3,11 @@
 set -eu
 
 ARCH=$(uname -m)
+DEB_LINK="https://github.com/Stirling-Tools/Stirling-PDF/releases/latest/download/Stirling-PDF-linux-$ARCH.deb"
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm webkit2gtk-4.1
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -15,4 +16,19 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
-# If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+ar xvf /tmp/app.deb
+tar -xvf ./data.tar.gz
+rm -f ./*.gz
+mv -v ./usr ./AppDir
+mv -v ./AppDir/share/applications/Stirling-PDF.desktop            ./AppDir
+mv -v ./AppDir/share/icons/hicolor/192x192/apps/stirling-pdf.png  ./AppDir/.DirIcon
+mv -v ./AppDir/share/icons/hicolor/192x192/apps/stirling-pdf.png  ./AppDir
+
+awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
