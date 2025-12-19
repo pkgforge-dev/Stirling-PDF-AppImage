963e60330c88938a52311730689856bdb5ca3134
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-19 17:07:33 -0400

    oops

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun \
-	./AppDir/bin/stirling-pdf
+	./AppDir/bin/stirling-pdf \
 	./AppDir/lib/Stirling-PDF
 
 # Additional changes can be done in between here
